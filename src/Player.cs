a5ddf95fa1799aa845e44d6aef977bbcbb458552
Michael Heilmann <mheilman@ist.ucf.edu>
2014-12-04 13:11:07 -0500

    listing recorded sessions now functional

--- src/Player.cs
+++ src/Player.cs
@@ -1,6 +1,9 @@
 using System;
 using OpenSim.Region.Framework.Scenes;
 using OpenSim.Region.Framework.Interfaces;
+using System.Collections.Generic;
+using OpenMetaverse;
+using OpenSim.Framework;
 
 namespace MOSES.AAR
 {
@@ -41,8 +44,8 @@ namespace MOSES.AAR
 		}
 		public void registerCommands(IRegionModuleBase regionModule, Scene scene)
 		{
-			scene.AddCommand("Aar", regionModule,"aar off","off","turn scripts off",haltScripts);
-			scene.AddCommand("Aar", regionModule,"aar on", "on", "turn scripts on",resumeScripts);
+			//scene.AddCommand("Aar", regionModule,"aar off","off","turn scripts off",haltScripts);
+			//scene.AddCommand("Aar", regionModule,"aar on", "on", "turn scripts on",resumeScripts);
 			scene.AddCommand("Aar", regionModule,"aar list","list","list recorded sessions", listSessions);
 
 		}
@@ -61,10 +64,34 @@ namespace MOSES.AAR
 
 		public void listSessions(string module, string[] args)
 		{
-
+			Dictionary<UUID,int> sessions = new Dictionary<UUID,int>();
+			foreach(TaskInventoryItem eb in aarBox.RootPart.Inventory.GetInventoryItems())
+			{
+				string[] parts = eb.Name.Split(':');
+				if(parts[0] == "session")
+				{
+					UUID sessionId = new UUID(parts[1]);
+					int part = Convert.ToInt32(parts[2]);
+					if(sessions.ContainsKey(sessionId))
+					{
+						if(sessions[sessionId] < part)
+						{
+							sessions[sessionId] = part;
+						}
+					}
+					else
+					{
+						sessions[sessionId] = part;
+					}
+				}
+			}
+			foreach(UUID id in sessions.Keys)
+			{
+				log(string.Format("session: {0}", id));
+			}
 		}
 
-		public void haltScripts(string module, string[] args)
+		private void haltScripts(string module, string[] args)
 		{
 			dialog.SendGeneralAlert("AAR Module: Halting scripts in preparation for Playback");
 			EntityBase[] ents = m_scene.Entities.GetEntities();
@@ -78,7 +105,7 @@ namespace MOSES.AAR
 				}
 			}
 		}
-		public void resumeScripts(string module, string[] args)
+		private void resumeScripts(string module, string[] args)
 		{
 			dialog.SendGeneralAlert("AAR Module: Restarting scripts after playback complete");
 			//this reloads scripts, it may reload all assemblies, but it works reliably
